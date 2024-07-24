fcd195abaa245e9284d4d59bb3cb8c574ec5271b
AleksaAlesya <a.n.brakor@gmail.com>
2024-07-24 16:55:35 +0300

    -отрицательные значения не допускает

--- src/main/java/by/alexandrabrakor/InfixToPostfix.java
+++ src/main/java/by/alexandrabrakor/InfixToPostfix.java
@@ -7,9 +7,8 @@ import java.util.regex.Pattern;
 
 public  class InfixToPostfix {
     private static final StringBuilder mathPostfix = new StringBuilder();
-    private static final LinkedList<String> operators = new LinkedList<>();
-    //храним все существующие операторы
-    private static final Stack<String> operatorsStack = new Stack<>(); // временно б. хранить отложенные операторы
+    private static final LinkedList<String> operators = new LinkedList<>();//храним все существующие операторы
+    private static final Stack<String> operatorsStack = new Stack<>();// временно б. хранить отложенные операторы
 
     static {
         operators.add("*");
@@ -20,6 +19,16 @@ public  class InfixToPostfix {
     }
 
 
+/* Берем строку с инфиксной записью выражения и переводим в постфиксную запись
+     f_27*6/(4+1) -  инфиксная запись
+     27 6 * 4 1 + / - постфиксная запись
+* последоватеельно каждый элемент сравниваем с Рег. Выр, пока нахоодим подходящий кусок, продолжаем
+* Ести это число, то мы добавляем его сразу в стринбилдер, в котором формируем  постфиксоное выражение и пробел
+* Если это  открыв. скобка ( - то мы ее добаляем в Стэк ( для временнного хранения операторов
+* Если это закрывающаяся скобка ), то мы берем все операторы из стэка до открыв. скобки ( записываем в стрингбилдер постф.  и удаляем  из стэка, скобку ( - удаляем
+* Если пришло, что то др. , т.е. операторы, и если в стеке пусто - то записываем с Стэк. Если не пусто, то достаем последний оператор и проверям их приоритеты, если у имеющегося выше приоритет, то его добавляем в стрингбилдер и удаляем из стэка, а этот записываем в стэк. Если же ниже, то этот добавляем в стэк
+* и так проходим по циклу, пока не будет пустой стэк
+* В итоге получаем пустой стэк и  постфиксное выражение*/
 
     public static String convert(String mathInfix) {
         operatorsStack.clear();
@@ -99,29 +108,28 @@ public  class InfixToPostfix {
        return sb.append("])").toString();
     }
 
-    //Регулярное выражение с цифрами
-    //числа.. в любом количестве, может быть точка и после точки любое количество чисел...т.е. вечественные числа тоже подойдут
-    //        \d    любая  цифра [0-9]
-    ////         +    любое количеств
-    ////        (  )  еще может что-то быть
-    ////        ()?   но может и не быть
-    ////        \.    может быть точка
-    ////         \d   цифры после точки
-    ////         +    любое количество
+    /*Регулярное выражение с цифрами
+    числа.. в любом количестве, может быть точка и после точки любое количество чисел...т.е. вечественные числа тоже подойдут
+         \d    любая  цифра [0-9]
+          +    любое количеств
+         (  )  еще может что-то быть
+         ()?   но может и не быть
+         \.    может быть точка
+         \d   цифры после точки
+          +    любое количество*/
     private static String getPatternStringNumbers() {
         return "(\\d+((\\.\\d+)?)+)";
     }
 
-    public static void main(String[] args) {
-        System.out.println(getPatternStringOperators());
-        System.out.println(getPatternStringBrackets());
-        System.out.println(getPatternStringNumbers());
-        System.out.println(getPatternStrings());
-        System.out.println(convert("f_27*6/(4+1)"));
-        System.out.println(convert("5+3"));
-        System.out.println(convert("5+5*45-(7-17)*8+6*9"));
-    }
+//    //Для проверки
+//    public static void main(String[] args) {
+//        System.out.println(getPatternStringOperators());
+//        System.out.println(getPatternStringBrackets());
+//        System.out.println(getPatternStringNumbers());
+//        System.out.println(getPatternStrings());
+//        System.out.println(convert("f_27*6/(4+1)"));
+//        System.out.println(convert("5+3"));
+//        System.out.println(convert("5+5*45-(7-17)*8+6*9"));
+//    }
 
 }
-//    f_27*6/(4+1) -  инфиксная запись
-//       27 6 * 4 1 + / - постфиксная запись
